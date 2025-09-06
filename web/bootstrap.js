a455b63f8a935c5a0d8ecd992e49671e4fa006cd
Ofer Chen <oferchen@gmail.com>
2025-09-07 00:22:23 +0200

    feat(web): add canvas and startup hooks

--- web/bootstrap.js
+++ web/bootstrap.js
@@ -1,7 +1,8 @@
 async function init() {
+  const canvasId = window.BEVY_CANVAS_ID || "bevy-canvas";
   const wasm = await import("./pkg/client.js");
   if (wasm && wasm.default) {
-    await wasm.default();
+    await wasm.default(canvasId);
   }
 }
 
