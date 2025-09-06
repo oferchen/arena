ffa9b06ae030ee42e812789034b32392dd4419d0
Ofer Chen <oferchen@gmail.com>
2025-09-06 19:06:40 +0200

    feat: versioned cache for service worker

--- web/sw.js
+++ web/sw.js
@@ -1,10 +1,27 @@
+const MANIFEST_VERSION = "__PRECACHE_VERSION__";
+const PRECACHE = `precache-${MANIFEST_VERSION}`;
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("precache").then(async (cache) => {
+    caches.open(PRECACHE).then(async (cache) => {
       const res = await fetch("/assets/precache.json");
       const files = await res.json();
       return cache.addAll(files);
     }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== PRECACHE) {
+            return caches.delete(key);
+          }
+        }),
+      ),
+    ),
   );
 });
 
